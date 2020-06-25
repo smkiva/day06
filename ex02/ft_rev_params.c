e86718cbadaa8f02e1ce7cf87a6862dc9ed52c79
Sinethemba Mkiva <smkiva@c3r4s3.wethinkcode.co.za>
2020-06-25 16:37:26 +0200

    day06

--- ex02/ft_rev_params.c
+++ ex02/ft_rev_params.c
@@ -1,32 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*   ft_rev_params.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smkiva <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/06/25 15:14:53 by smkiva            #+#    #+#             */
-/*   Updated: 2020/06/25 16:15:43 by smkiva           ###   ########.fr       */
+/*   Created: 2020/06/25 16:30:20 by smkiva            #+#    #+#             */
+/*   Updated: 2020/06/25 16:34:36 by smkiva           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include<unistd.h>
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
 	write(1, &c, 1);
 }
+
 int		main(int argc, char  *argv[])
 {
 	int i;
+	int j;
 
-	i = 0;
-	if (argc > 0)
+	j = 1;
+	while (j < argc)
 	{
-		while (argv[0][i] != '\0')
+		i = 0;
+		while (argv[j][i] != '\0')
 		{
-			ft_putchar(argv[0][i]);
+			ft_putchar(argv[j][i]);
 			++i;
 		}
 		ft_putchar('\n');
+		++j;
 	}
 	return (0);
 }
